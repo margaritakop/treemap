b4fabbd24c193e7252f51d8fcb92d0bc0daa9439
MargaritaKop <margaita.kopniczky@gmail.com>
2018-03-26 14:14:43 +0200

    clean some comments

--- scripts.js
+++ scripts.js
@@ -27,10 +27,8 @@ function mapLocation() {
 
   function calcRoute() {
         var start = new google.maps.LatLng(51.50346844984542, -0.18084555272162106);
-        //var end = new google.maps.LatLng(38.334818, -181.884886);
         var end = new google.maps.LatLng(52.000, -0.200);
-        /*
-*/
+
         var bounds = new google.maps.LatLngBounds();
         bounds.extend(start);
         bounds.extend(end);
