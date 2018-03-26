17634a8f6ed8a00e29f2959d5e65a742ae6bd86d
MargaritaKop <margaita.kopniczky@gmail.com>
2018-03-26 13:35:23 +0200

    rout
    based on https://stackoverflow.com/questions/27341214/how-to-draw-a-route-between-two-markers-in-google-maps
    
    modify to get the end lat and long from searchbox entry

--- scripts.js
+++ scripts.js
@@ -1,12 +1,51 @@
 
-function initMap() {
-  var uluru = {lat: 51.50346844984542, lng: -0.18084555272162106};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-     map: map
-  });
+function mapLocation() {
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+
+
+  function initialize() {
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        var uluru_tree = {lat: 51.50346844984542, lng: -0.18084555272162106};
+        var mapOptions = {
+            zoom: 10,
+            center: uluru_tree
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        var marker = new google.maps.Marker({
+          position: uluru_tree,
+          map: map
+        });
+                directionsDisplay.setMap(map);
+        google.maps.event.addDomListener(document.getElementById('routebtn'), 'click', calcRoute);
+    }
+
+  function calcRoute() {
+        var start = new google.maps.LatLng(51.50346844984542, -0.18084555272162106);
+        //var end = new google.maps.LatLng(38.334818, -181.884886);
+        var end = new google.maps.LatLng(51.000, -0.200);
+        /*
+*/
+        var bounds = new google.maps.LatLngBounds();
+        bounds.extend(start);
+        bounds.extend(end);
+        map.fitBounds(bounds);
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                directionsDisplay.setMap(map);
+            } else {
+                alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+            }
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 }
+mapLocation();
