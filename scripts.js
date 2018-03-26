00903f5923d919967fcbbf4cc4896add5e6dae34
MargaritaKop <margaita.kopniczky@gmail.com>
2018-03-26 11:35:34 +0200

    zoom in to L

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 function initMap() {
   var uluru = {lat: 51.50346844984542, lng: -0.18084555272162106};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 12,
     center: uluru
   });
   var marker = new google.maps.Marker({
