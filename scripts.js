105d5a2e3dfeb676e3a1f6d59adf32bb6b4241fd
MargaritaKop <margaita.kopniczky@gmail.com>
2018-03-26 10:15:56 +0200

    add coordinates for tree

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: 51.50346844984542, lng: -0.18084555272162106};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: uluru
