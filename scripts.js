d1c5b7c881b7efd15e2cea41be95cefec093186f
MargaritaKop <margaita.kopniczky@gmail.com>
2018-03-30 13:51:19 +0200

    get route from you

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ function mapLocation() {
     var directionsDisplay;
     var map;
     var latlong_tree = {lat: 51.50346844984542, lng: -0.18084555272162106};
+    var latlong_you = {lat: 46.2579605, lng: 20.155540100000003};
 
     google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -11,7 +12,7 @@ function mapLocation() {
         //initial map
         directionsDisplay = new google.maps.DirectionsRenderer();
         var mapOptions = {
-            zoom: 10,
+            zoom: 4,
             center: latlong_tree
         };
         map = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -29,45 +30,31 @@ function mapLocation() {
             };
             bootbox.alert('Your location: ' + String(position.coords.latitude) + ' ' + String(position.coords.longitude));
           }, function() {});
-        } 
+        }; 
 
         //transport buttons
         google.maps.event.addDomListener(document.getElementById('WALKING'), 'click', 
                   function() {
-                              transport = this.id
-                              getRout();
+                              transport = this.id;
+                              calcRoute(latlong_you);
                   });
         google.maps.event.addDomListener(document.getElementById('DRIVING'), 'click',
                           function() {
-                              transport = this.id
-                              getRout();
+                              transport = this.id;
+                              calcRoute(latlong_you);
                   });
         google.maps.event.addDomListener(document.getElementById('TRANSIT'), 'click',
                           function() {
-                              transport = this.id
-                              getRout();
+                              transport = this.id;
+                              calcRoute(latlong_you);
                   });
     }
 
-  function getRout(){  
-        var geocoder = new google.maps.Geocoder();
-        var address = document.getElementById('pac-input').value;
-        geocoder.geocode({ 'address': address }, function (results, status) {
-
-            if (status == google.maps.GeocoderStatus.OK) {
-                var lat_address = results[0].geometry.location.lat();
-                var long_address = results[0].geometry.location.lng();
-                calcRoute(lat_address, long_address);
-            }
-            else {bootbox.alert("Could not find coordinates for" + address + ".")}
-        });
-    }
-
-  function calcRoute(lat, long) {
+  function calcRoute(latlong_you) {
         //remove the original marker and define the new end-start points
         marker.setMap(null);
         var end = new google.maps.LatLng(latlong_tree);
-        var start = new google.maps.LatLng(lat, long);
+        var start = new google.maps.LatLng(latlong_you);
 
         //set new bounds to the map
         var bounds = new google.maps.LatLngBounds();
